77d496911b49a4c0b60418afdb3b028d4b870360
KRIS-LOPEZ\unkno <unknownlopez101@gmail.com>
2018-07-17 15:34:05 -0400

    added "/"

--- sendEmail.py
+++ sendEmail.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
--- testScript.py
+++ testScript.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
 from tkinter import *
 from firebase import firebase
